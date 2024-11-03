c4ff20f62d9245db7ccfb4cbd39690b8598bfe74
alhajerih <alhajerih@hotmail.com>
2024-11-03 14:46:47 +0300

    Task Done

---
+++ Task-List/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String>playlist=new ArrayList<>();
+        playlist.add("Hitman");
+        playlist.add("Batman");
+        playlist.add("Spiderman");
+        playlist.add("I beleive ");
+        playlist.add("Codey");
+        playlist.remove(2);
+
+        for (String play:playlist) System.out.println(play);
+
+        //for loop to print much time to the array size
+        for(int i = 0; i<playlist.size();i++){
+            System.out.println(playlist);
+        }
+
+
+    }
+}
